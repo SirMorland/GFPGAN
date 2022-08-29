b75dad0d402c4e4c90b18b065d60439b52391f2c
Ville Helppolainen <ville.helppolainen@gmail.com>
2022-08-30 00:18:28 +0300

    Make inference_gfpgan.py callable as function

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -1,59 +1,36 @@
 import argparse
+import base64
 import cv2
 import glob
 import numpy as np
 import os
 import torch
+from dataclasses import dataclass
 from basicsr.utils import imwrite
 
 from gfpgan import GFPGANer
 
+@dataclass
+class Args:
+    version: str
+    upscale: int
+    bg_upsampler: str
+    bg_tile: int
+    only_center_face: bool
+    aligned: bool
 
-def main():
+async def fix_faces(data, sio, sid):
     """Inference demo for GFPGAN (for users).
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-i',
-        '--input',
-        type=str,
-        default='inputs/whole_imgs',
-        help='Input image or folder. Default: inputs/whole_imgs')
-    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder. Default: results')
-    # we use version to select models, which is more user-friendly
-    parser.add_argument(
-        '-v', '--version', type=str, default='1.3', help='GFPGAN model version. Option: 1 | 1.2 | 1.3. Default: 1.3')
-    parser.add_argument(
-        '-s', '--upscale', type=int, default=2, help='The final upsampling scale of the image. Default: 2')
 
-    parser.add_argument(
-        '--bg_upsampler', type=str, default='realesrgan', help='background upsampler. Default: realesrgan')
-    parser.add_argument(
-        '--bg_tile',
-        type=int,
-        default=400,
-        help='Tile size for background sampler, 0 for no tile during testing. Default: 400')
-    parser.add_argument('--suffix', type=str, default=None, help='Suffix of the restored faces')
-    parser.add_argument('--only_center_face', action='store_true', help='Only restore the center face')
-    parser.add_argument('--aligned', action='store_true', help='Input are aligned faces')
-    parser.add_argument(
-        '--ext',
-        type=str,
-        default='auto',
-        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs. Default: auto')
-    args = parser.parse_args()
-
-    args = parser.parse_args()
-
-    # ------------------------ input & output ------------------------
-    if args.input.endswith('/'):
-        args.input = args.input[:-1]
-    if os.path.isfile(args.input):
-        img_list = [args.input]
-    else:
-        img_list = sorted(glob.glob(os.path.join(args.input, '*')))
-
-    os.makedirs(args.output, exist_ok=True)
+    args = Args(
+        version="1.3",
+        upscale=1,
+        bg_upsampler="realesrgan",
+        bg_tile=400,
+        only_center_face=False,
+        aligned=False,
+    )
 
     # ------------------------ set up background upsampler ------------------------
     if args.bg_upsampler == 'realesrgan':
@@ -94,7 +71,7 @@ def main():
         raise ValueError(f'Wrong model version {args.version}.')
 
     # determine model paths
-    model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
+    model_path = os.path.join('../GFPGAN/experiments/pretrained_models', model_name + '.pth')
     if not os.path.isfile(model_path):
         model_path = os.path.join('realesrgan/weights', model_name + '.pth')
     if not os.path.isfile(model_path):
@@ -108,48 +85,16 @@ def main():
         bg_upsampler=bg_upsampler)
 
     # ------------------------ restore ------------------------
-    for img_path in img_list:
-        # read image
-        img_name = os.path.basename(img_path)
-        print(f'Processing {img_name} ...')
-        basename, ext = os.path.splitext(img_name)
-        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    # read image
+    encoded = data.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded), np.uint8)
+    input_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        # restore faces and background if necessary
-        cropped_faces, restored_faces, restored_img = restorer.enhance(
-            input_img, has_aligned=args.aligned, only_center_face=args.only_center_face, paste_back=True)
+    # restore faces and background if necessary
+    cropped_faces, restored_faces, restored_img = restorer.enhance(
+        input_img, has_aligned=args.aligned, only_center_face=args.only_center_face, paste_back=True)
 
-        # save faces
-        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
-            # save cropped face
-            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
-            imwrite(cropped_face, save_crop_path)
-            # save restored face
-            if args.suffix is not None:
-                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
-            else:
-                save_face_name = f'{basename}_{idx:02d}.png'
-            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
-            imwrite(restored_face, save_restore_path)
-            # save comparison image
-            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
-            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
-
-        # save restored img
-        if restored_img is not None:
-            if args.ext == 'auto':
-                extension = ext[1:]
-            else:
-                extension = args.ext
-
-            if args.suffix is not None:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
-            else:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}.{extension}')
-            imwrite(restored_img, save_restore_path)
-
-    print(f'Results are in the [{args.output}] folder.')
-
-
-if __name__ == '__main__':
-    main()
+    # save restored img
+    if restored_img is not None:
+        success, encoded_image = cv2.imencode(".png", restored_img)
+        return encoded_image.tobytes()
